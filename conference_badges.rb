22f439911e2f74e22ef442e5d568096c6f176132
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-03 16:34:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages << badge_maker(name)
+  end
+  messages
+end
